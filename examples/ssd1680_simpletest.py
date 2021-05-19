3146761d5b75f7c623f1a9e224a2d8f14cf25c1c
dherrada <dylan.herrada@adafruit.com>
2021-05-19 14:31:30 -0400

    Linted

--- examples/ssd1680_simpletest.py
+++ examples/ssd1680_simpletest.py
@@ -41,16 +41,15 @@ display = adafruit_ssd1680.SSD1680(
 
 g = displayio.Group()
 
-f = open("/display-ruler.bmp", "rb")
+with open("/display-ruler.bmp", "rb") as f:
+    pic = displayio.OnDiskBitmap(f)
+    t = displayio.TileGrid(pic, pixel_shader=displayio.ColorConverter())
+    g.append(t)
 
-pic = displayio.OnDiskBitmap(f)
-t = displayio.TileGrid(pic, pixel_shader=displayio.ColorConverter())
-g.append(t)
+    display.show(g)
 
-display.show(g)
+    display.refresh()
 
-display.refresh()
+    print("refreshed")
 
-print("refreshed")
-
-time.sleep(120)
+    time.sleep(120)
